6f853e17a7933b1848a9da15d70517fc5f36b315
evb0110 <7149553@gmail.com>
2018-12-11 13:02:46 +0300

    switched to reduce() from forEach() (go functional!)

--- 2016/001_No Time for a Taxicab/notime.js
+++ 2016/001_No Time for a Taxicab/notime.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 
 const input = fs.readFileSync('input.txt', 'utf-8')
-                .split(',').map(el => el.trim());
+  .split(',').map(el => el.trim());
 
-let directionIndex = 0; // 0123 => NESW
+let directionIndex = 0; 
+  // 0123 => NorthEastSouthWest
 let coordinates = [0, 0];
 
 const calculateInstructions = (input) => {
@@ -11,8 +12,7 @@ const calculateInstructions = (input) => {
   input.forEach(el => {
     const rotation = el[0];
     const number = + el.slice(1);
-    
-    instructions.push({rotation, number})
+    instructions.push({ rotation, number })
   })
   return instructions;
 }
@@ -21,21 +21,27 @@ const makeStep = (directionIndex, coordinates, instruction) => {
   ({ rotation, number } = instruction);
   directionIndex += (rotation == 'R') ? 1 : 3;
   directionIndex = directionIndex % 4;
-  
+
   switch (directionIndex) {
     case 0: coordinates[1] += number; break;
     case 1: coordinates[0] += number; break;
     case 2: coordinates[1] -= number; break;
     case 3: coordinates[0] -= number; break;
   }
-  return {directionIndex, coordinates};
- }
+
+  return { directionIndex, coordinates };
+}
 
 const instructions = calculateInstructions(input);
 
-instructions.forEach((instruction) => {
-  ({directionIndex, coordinates} = 
-    makeStep(directionIndex, coordinates, instruction));
-})
 
-console.log(coordinates.reduce((a,b) => a+b)); 
+let result = instructions.reduce(
+  (acc, instruction) => makeStep(
+    acc.directionIndex, acc.coordinates, instruction
+  ),
+  ({ directionIndex, coordinates })
+);
+
+({coordinates} = result);
+
+console.log(coordinates.map(num => Math.abs(num)).reduce((a,b) => a + b));
