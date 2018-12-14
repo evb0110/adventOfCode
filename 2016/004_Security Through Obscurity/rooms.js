f64322f1021c1977f617b12773922bab2cb3a978
evb0110 <7149553@gmail.com>
2018-12-14 14:57:31 +0300

    solved part 1 of 004

--- 2016/004_Security Through Obscurity/rooms.js
+++ 2016/004_Security Through Obscurity/rooms.js
@@ -7,12 +7,10 @@ const splitRoom = (line) => {
   const idRegex = /[0-9]+/;
   const checksumRegex = /\[\S+\]/;
 
-
   const left = line.match(leftRegex)[0].replace(/-/g, '');
   const id = + line.match(idRegex)[0];
   const checksum = line.match(checksumRegex)[0].replace(/\[|\]/g, '');
 
- 
   return [...[left, id, checksum]];
 }
 
@@ -22,8 +20,31 @@ const histogram = (line) => {
   letters.forEach(letter => {
     frequencies[letter] = frequencies[letter] + 1 || 1;
   });
-  return {...frequencies};
+  return { ...frequencies };
 }
 
-let res = splitRoom('vxupkizork-sgmtkzoi-pkrrehkgt-zxgototm-644[kotgr]')[0];
-console.log(histogram(res));
+const histogramToArray = (frequencies) => {
+  return Object.keys(frequencies).sort((a, b) => {
+      a_ = -frequencies[a];
+      b_ = -frequencies[b];
+    if (a_ == b_) {
+      a_ = a.charCodeAt();
+      b_ = b.charCodeAt();
+    }
+    return (a_ - b_) / Math.abs(a_ - b_);
+  })
+}
+
+const proceedRoom = (currentSum, room) => {
+  ([left, id, checksum] = splitRoom(room));
+  const myHistogram = histogram(left);
+  const arr = histogramToArray(myHistogram);
+  const myChecksum = arr.slice(0, 5).join('');
+  return (myChecksum == checksum) 
+    ? currentSum + id
+    : currentSum;
+}
+
+let res = input.reduce((acc, el) => proceedRoom(acc, el), 0);
+
+console.log(res);
