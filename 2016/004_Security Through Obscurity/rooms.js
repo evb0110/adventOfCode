75b3550a9d7d8ac4ea848f66c5c44952f2deacde
evb0110 <7149553@gmail.com>
2018-12-14 15:02:51 +0300

    cleaned code, encapsulated functions

--- 2016/004_Security Through Obscurity/rooms.js
+++ 2016/004_Security Through Obscurity/rooms.js
@@ -1,50 +1,49 @@
 const fs = require('fs');
-
 const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
 
-const splitRoom = (line) => {
-  const leftRegex = /^[^0-9]+/;
-  const idRegex = /[0-9]+/;
-  const checksumRegex = /\[\S+\]/;
+const proceedRoom = (currentSum, room) => {
+  const splitRoom = (line) => {
+    const leftRegex = /^[^0-9]+/;
+    const idRegex = /[0-9]+/;
+    const checksumRegex = /\[\S+\]/;
 
-  const left = line.match(leftRegex)[0].replace(/-/g, '');
-  const id = + line.match(idRegex)[0];
-  const checksum = line.match(checksumRegex)[0].replace(/\[|\]/g, '');
+    const left = line.match(leftRegex)[0].replace(/-/g, '');
+    const id = + line.match(idRegex)[0];
+    const checksum = line.match(checksumRegex)[0].replace(/\[|\]/g, '');
 
-  return [...[left, id, checksum]];
-}
+    return [...[left, id, checksum]];
+  }
 
-const histogram = (line) => {
-  const letters = line.split('');
-  let frequencies = {};
-  letters.forEach(letter => {
-    frequencies[letter] = frequencies[letter] + 1 || 1;
-  });
-  return { ...frequencies };
-}
+  const histogram = (line) => {
+    const letters = line.split('');
+    let frequencies = {};
+    letters.forEach(letter => {
+      frequencies[letter] = frequencies[letter] + 1 || 1;
+    });
+    return {...frequencies};
+  }
 
-const histogramToArray = (frequencies) => {
-  return Object.keys(frequencies).sort((a, b) => {
+  const histogramToArray = (frequencies) => {
+    return Object.keys(frequencies).sort((a, b) => {
       a_ = -frequencies[a];
       b_ = -frequencies[b];
-    if (a_ == b_) {
-      a_ = a.charCodeAt();
-      b_ = b.charCodeAt();
-    }
-    return (a_ - b_) / Math.abs(a_ - b_);
-  })
-}
+      if (a_ == b_) {
+        a_ = a.charCodeAt();
+        b_ = b.charCodeAt();
+      }
+      return (a_ - b_) / Math.abs(a_ - b_);
+    })
+  }
 
-const proceedRoom = (currentSum, room) => {
-  ([left, id, checksum] = splitRoom(room));
+  [left, id, checksum] = splitRoom(room);
   const myHistogram = histogram(left);
   const arr = histogramToArray(myHistogram);
   const myChecksum = arr.slice(0, 5).join('');
-  return (myChecksum == checksum) 
+  return (myChecksum == checksum)
     ? currentSum + id
     : currentSum;
 }
 
-let res = input.reduce((acc, el) => proceedRoom(acc, el), 0);
+const res = input.reduce((acc, el) => proceedRoom(acc, el), 0);
 
 console.log(res);
