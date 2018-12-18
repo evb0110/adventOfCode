3b9da26b1b7cdeb5009301e6dc1e4aab19babc3a
evb0110 <7149553@gmail.com>
2018-12-18 14:07:36 +0300

    more code cleanup for 007.2

--- 2016/007_Internet Protocol Version 7/protocol2-functional.js
+++ 2016/007_Internet Protocol Version 7/protocol2-functional.js
@@ -16,23 +16,23 @@ const aba = (trio) => {
   if (trio.length !== 3) {
     throw `wrong trio`;
   }
-  let verdict = (trio[0] == trio[2]) && (trio[0] != trio[1]);
+  let verdict = (trio[0] == trio[2]) && (trio[0] !== trio[1]);
   return !!verdict;
 }
 
 const abaTrios = (line) => {
-  return trios(line).filter((trio) => aba(trio))
+  return trios(line).filter(trio => aba(trio))
 }
 
 const abaToBab = (trio) => {
   if ((trio.length !== 3) || !(aba(trio))) {
     throw `wrong trio`;
   }
-  return trio[1] + trio[0] + trio[1];
+  return '' + trio[1] + trio[0] + trio[1];
 }
 
-const intersection = ( set1, set2 ) => {
-  const intersection = [...set1].filter(x => (set2.indexOf(x) + 1));
+const intersection = ( arr1, arr2 ) => {
+  const intersection = [...arr1].filter(x => (arr2.indexOf(x) + 1));
   return intersection;
 }
 
@@ -49,8 +49,7 @@ const result = input.reduce((acc, line) => {
   badParts.forEach((line) => {
     badTrios.push(...abaTrios(line));
   })
-
-  const badTriosTransformed = badTrios.map((trio) => abaToBab(trio));
+  const badTriosTransformed = badTrios.map(trio => abaToBab(trio));
   const goodBadIntersection = intersection(goodTrios, badTriosTransformed);
   if(goodBadIntersection.length) counter++;
   return counter;
