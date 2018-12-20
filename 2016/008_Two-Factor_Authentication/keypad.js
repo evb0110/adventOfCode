00ca69bc414287802cd541c573274c7159b81b73
evb0110 <7149553@gmail.com>
2018-12-20 19:03:02 +0300

    008.1 still wrong

--- 2016/008_Two-Factor_Authentication/keypad.js
+++ 2016/008_Two-Factor_Authentication/keypad.js
@@ -1,15 +1,19 @@
 const R = require('ramda');
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
+
+
 
 // Initializing a 2-dimensional array
-const width = 50;
+const width = 50
 const height = 6;
 
-const screen = Array(width)
+const initScreen = Array(width)
 for (i = 0; i < width; i++)
-  screen[i] = Array(height).fill(false);
+  initScreen[i] = Array(height).fill(false);
 
-
-const rect = (screen, A, B) => {
+const rect = (A, B, screen) => {
   const newScreen = screen.map((col, x) => {
     return col.map((el, y) => {
       if ((x < A) && (y < B)) return true;
@@ -19,34 +23,61 @@ const rect = (screen, A, B) => {
   return newScreen;
 }
 
-const roty = (screen, A, B) => {
-  const yrot = A - 1;
+const roty = (A, B, screen) => {
+  const yrot = A;
   const newScreen = screen.map((col, x) => {
     return col.map((el, y) => {
       if (y !== yrot) return el;
-      const xPrev = (x - B + width) % (width - 1);
+      const xPrev = (x - B + width) % width;
       return screen[xPrev][y];
     })
   })
   return newScreen;
 }
 
-const rotx = (screen, A, B) => {
-  const xrot = A - 1;
+const rotx = (A, B, screen) => {
+
+  const xrot = A;
   const newScreen = screen.map((col, x) => {
     return col.map((el, y) => {
       if (x !== xrot) return el;
-      const yPrev = (y - B + width) % (width - 1);
-      return screen[x][yPrev];
+      
+      const yPrev = (y - B + height) % height;
+       return screen[x][yPrev];
     })
   })
   return newScreen;
 }
 
-const dispatch = (line => {
-  
+[RECT, ROTX, ROTY] =
+  [rect, rotx, roty].map((fn) => {
+    return R.curry(fn)
 })
 
-console.log(rotx(rect(screen, 2, 2), 1, 2));
+const dispatch = (line => {
+  const [A, B] = line.match(/\d+/g);
+  if (line.includes("rect")) return [RECT, A, B];
+  if (line.includes("column")) return [ROTX, A, B];
+  if (line.includes("row")) return [ROTY, A, B];
+})
+
+const result = input.reduce((acc, line) => {
+  const currentScreen = acc;
+  const [fn, A, B] = dispatch(line);
+  const newScreen = fn(A, B, currentScreen);
+  return newScreen;
+}, initScreen);
+
+console.log(result.toString().split(',').filter(a => a == 'true').length);
+
+
+// console.log(
+//   R.compose(
+//     ROTX(1, 1),
+//     ROTY(0, 4),
+//     ROTX(1, 1), 
+//     RECT(3, 2),
+//     )(screen)
+//   );
 
 
